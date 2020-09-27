79ab035e4e1a74a5066923f6fbbcc0575d91e681
Johson Rodriguez <sukerjc@gmail.com>
2020-09-27 15:38:00 -0500

    sunday2lls

--- src/main/java/com/project/market/persistence/mapper/CategoryMapper.java
+++ src/main/java/com/project/market/persistence/mapper/CategoryMapper.java
@@ -3,20 +3,24 @@ package com.project.market.persistence.mapper;
 
 import com.project.market.domain.Category;
 import com.project.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface CategoryMapper {
     //Mapeamos los campos de español a ingles
 
     @Mappings({
             @Mapping(source = "idCategoria", target = "categoryId"),
-            @Mapping(source = "idCategoria", target = "categoryId"),
-            @Mapping(source = "idCategoria", target = "categoryId"),
-            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
     })
     Category toCategory(Categoria categoria);
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
 
 }
