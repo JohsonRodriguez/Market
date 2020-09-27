879d13f24420436eec15b551c400008602facef5
Johson Rodriguez <sukerjc@gmail.com>
2020-09-27 11:26:34 -0500

    sunday

--- src/main/java/com/project/market/persistence/ProductoRepository.java
+++ src/main/java/com/project/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.project.market.persistence;
 
 import com.project.market.persistence.crud.ProductoCrudRepository;
 import com.project.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository//indicamos a spring que esta clase interactuará con la base de datos
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +22,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    public Optional<Producto> getProducto(int  idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
