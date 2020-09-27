9a8560a99ebd0b0090b8d94f29fd8a7607e3a213
Johson Rodriguez <sukerjc@gmail.com>
2020-09-26 21:49:45 -0500

    clase cliente 3

--- src/main/java/com/project/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/project/market/persistence/entity/ComprasProducto.java
@@ -3,13 +3,48 @@ package com.project.market.persistence.entity;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "COMPRAS_PRODUCTOS")
 public class ComprasProducto {
 
     @EmbeddedId //id generado de la tabla serialiable
-    
-private  ComprasProductoPk id;
 
+    private  ComprasProductoPk id;
+    private Integer cantidad;
+    private BigDecimal total;
+    private boolean estado;
+
+    public ComprasProductoPk getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPk id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
