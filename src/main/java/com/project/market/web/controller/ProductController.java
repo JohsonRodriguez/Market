6a2e967142de12bdea1419a71ea9c9155e6e926a
Johson Rodriguez <sukerjc@gmail.com>
2020-09-28 12:49:31 -0500

    tmr

--- src/main/java/com/project/market/web/controller/ProductController.java
+++ src/main/java/com/project/market/web/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -15,6 +16,23 @@ public class ProductController {
     private ProductService productService;
 
     public List<Product> getAll(){
-        
+        return productService.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productService.getByCategory(categoryId);
+
+    }
+
+    public Product save(Product product){
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId){
+        return productService.delete(productId);
     }
 }
